995c7d93084ea16850b93aa74ca4c756cc411154
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2020-03-08 17:49:38 -0700

    Add files via upload

---
+++ Activity3-1/Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using Activity3_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity3_1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        Customer customer;
+        List<Customer> customers;  
+
+        public CustomerController()
+        {
+            customers = new List<Customer>();
+            customers.Add(new Customer(0, "Mickey", 20));
+            customers.Add(new Customer(1, "Chael", 19));
+            customers.Add(new Customer(2, "Kylie", 23));
+            customers.Add(new Customer(3, "Ashley", 24));
+            customers.Add(new Customer(4, "Shaley", 21));
+            customers.Add(new Customer(5, "Shawn", 20));
+        }
+
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            Tuple<List<Customer>, Customer> tuple;
+
+            tuple = new Tuple<List<Customer>, Customer>(customers, customers[0]);
+
+            return View("Customer", tuple);
+        }
+
+        [HttpPost]
+        public ActionResult OnSelectCustomer(string customerNumber )
+        {
+            Tuple<List<Customer>, Customer> tuple; 
+
+            tuple = new Tuple<List<Customer>, Customer>(customers, customers[Int32.Parse(customerNumber)]);
+
+            return PartialView("_CustomerDetails", customers[Int32.Parse(customerNumber)]);
+        }
+    }
+}
