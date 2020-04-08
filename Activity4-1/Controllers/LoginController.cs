a3426ff94da89cf1efc1a2b06c45d2224a74cc33
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2020-04-07 22:14:09 -0700

    Add files via upload

---
+++ Activity4-1/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using Activity1_3.Models;
+using Activity1_3.Services.Business;
+using Activity1_3.Services.Utility;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace Activity1_3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel user)
+        {
+            MyLogger.GetInstance().Info("Entering LoginController.Login()");
+            MyLogger.GetInstance().Info("Parameters are: new JavaScriptSerializer().Serialize(user)");
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View("Login");
+                }
+
+                //return "Here " + model.Username + " and " + model.Password;
+                SecurityService ss = new SecurityService();
+                Boolean success = ss.Authenticate(user);
+
+                if (success)
+                {
+                    MyLogger.GetInstance().Info("Exiting LoginController.Login() with login passing");
+                    return View("LoginSuccess", user);
+                }
+                else
+                {
+                    MyLogger.GetInstance().Info("Exiting LoginController.Login() with login failing");
+                    return View("LoginFail");
+                }
+            }
+            catch(Exception e)
+            {
+                MyLogger.GetInstance().Error("Exception LoginController.Login()", e.Message);
+                return View("LoginError"); 
+            }
+        }
+    }
+}
---
+++ Activity4-1/Services/Utility/MyLogger.cs
@@ -0,0 +1,76 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activity1_3.Services.Utility
+{
+    public class MyLogger : ILogger
+    {
+        //singleton pattern example; only one instance of this class can be instanciated 
+        private static MyLogger instance; //singleton design pattern 
+        private static Logger logger; //static variable to hold a single instance of the nLog logger
+
+        //single pattern design - private constructor 
+        private MyLogger()
+        {
+            
+        }
+
+
+        //single pattern design - this function creates an instance of a class if it has not been instantiated
+        //If the class already exists in the program, then send them the reference to the original
+        public static MyLogger GetInstance()
+        {
+            if (instance == null)
+                instance = new MyLogger();
+            return instance; 
+        }
+
+        //private helper method that gets an instance of the NLog logger
+        private Logger GetLogger(string theLogger)
+        {
+            if (MyLogger.logger == null)
+                MyLogger.logger = LogManager.GetLogger(theLogger);
+            return MyLogger.logger;    
+        }
+
+
+        //ILogger interface implementation: Debug method with optional second argument for message
+        public void Debug(string message, string arg = null)
+        {
+            if (arg == null)
+                GetLogger("myAppLoggerRules").Debug(message);
+            else
+                GetLogger("myAppLoggerRules").Debug(message, arg); 
+        }
+
+        //ILogger interface implementation: Error method with optional second argument for message
+        public void Error(string message, string arg = null)
+        {
+            if (arg == null)
+                GetLogger("myAppLoggerRules").Error(message);
+            else
+                GetLogger("myAppLoggerRules").Error(message, arg);
+        }
+
+        //ILogger interface implementation: Info method with optional second argument for message
+        public void Info(string message, string arg = null)
+        {
+            if (arg == null)
+                GetLogger("myAppLoggerRules").Info(message);
+            else
+                GetLogger("myAppLoggerRules").Info(message, arg);
+        }
+
+        //ILogger interface implementation: Warn method with optional second argument for message
+        public void Warning(string message, string arg = null)
+        {
+            if (arg == null)
+                GetLogger("myAppLoggerRules").Warn(message);
+            else
+                GetLogger("myAppLoggerRules").Warn(message, arg);
+        }
+    }
+}
